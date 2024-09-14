dc90f5be3e2d88be424f3e6122f49009a0aa1c86
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2024-09-14 13:53:55 +0200

    Main structure of MainContent component is done, no functionality yet -still have to create the children components.

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useOnboarding } from "../context/OnboardingContext";
 
 function MainContent() {
-  return <div>MainContent</div>;
+  const { currentStep, steps, isSubmitted, isAnimating } = useOnboarding();
+  return (
+    <div className="flex-1 p-10">
+      <div className="max-w-md mx-auto">
+        {!isSubmitted ? (
+          // add animation to this div later
+          <div className="border-2 border-red-500">
+            <h1 className="text-3xl font-bol mb-3">{steps[currentStep].question}</h1>
+            {/* if current step is smaller than steps.length-1 add stepcontent componenet otherwise display summary */}
+            {/* add navigation component */}
+          </div>
+        ) : (
+          // if isSubmited, display Succes component.
+          <p>a</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default MainContent;
