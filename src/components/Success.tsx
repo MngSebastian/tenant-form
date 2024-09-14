52a7e43051292a296249d0c9bd51053b5942462e
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2024-09-15 01:08:17 +0200

    feat: Completed Success component

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { CheckCircle } from "lucide-react";
 
 function Success() {
-  return <div>Success</div>;
+  return (
+    <div>
+      <CheckCircle className="w-16 h-16 text-green-500 mx-auto mb-4 animate-pulse" />
+      <h2 className="text-2xl font-bol text-green-600 mb-2">Success.</h2>
+      <p className="text-gray-600">Your information has been submitted successfully.</p>
+    </div>
+  );
 }
 
 export default Success;
