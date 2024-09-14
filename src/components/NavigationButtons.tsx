ad651514bd296608cee1957475845d237a92905e
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2024-09-15 01:24:23 +0200

    feat: Added conditional render to Back Button to display cursor-not-allowed if page is on the first step

--- src/components/NavigationButtons.tsx
+++ src/components/NavigationButtons.tsx
@@ -8,8 +8,9 @@ function NavigationButtons() {
     <div className="flex justify-between mt-6">
       <button
         onClick={handlePrevious}
-        className="px-4 py-2 border border-gray-300 rounded-md bg-white transition duration-300 text-gray-600
-        hover:bg-gray-800 hover:text-white"
+        className={`px-4 py-2 border border-gray-300 rounded-md bg-white transition duration-300 text-gray-600
+         ${currentStep === 0 ? "cursor-not-allowed" : "hover:bg-gray-800 hover:text-white"}`}
+        disabled={currentStep === 0}
       >
         Back
       </button>
