d548efcdbca4f211bf0e5a0c63bf4d312653fcfc
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2024-09-14 13:38:02 +0200

    feat: added MainContent Component, currently empty.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import ProgressionBar from "./components/ProgressionBar";
+import MainContent from "./components/MainContent";
 function App() {
   return (
     <div className="md:flex w-screen h-screen bg-gray-100">
       <ProgressionBar />
+      <MainContent />
     </div>
   );
 }
