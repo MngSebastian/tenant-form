340e13f421a03ac8833961828ff607a600194cc6
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2024-09-15 18:12:17 +0200

    Stylist changes to Success component

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { CheckCircle } from "lucide-react";
-
+import { useOnboarding } from "../context/OnboardingContext";
 function Success() {
+  const { isDarkMode } = useOnboarding();
   return (
-    <div>
+    <div className="flex flex-col items-center w-full mt-20">
       <CheckCircle className="w-16 h-16 text-green-500 mx-auto mb-4 animate-pulse" />
       <h2 className="text-2xl font-bol text-green-600 mb-2">Success.</h2>
-      <p className="text-gray-600">Your information has been submitted successfully.</p>
+      <p className={`${isDarkMode ? "text-gray-300" : "text-gray-600"} `}>
+        Your information has been submitted successfully.
+      </p>
     </div>
   );
 }
