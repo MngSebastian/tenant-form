6c2a62d27939a4a11c9ab5f8a211cc803506a2e4
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2024-09-15 12:28:23 +0200

    feat: DarkMode styling added to MainContent component.

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -8,17 +8,18 @@ import Success from "./Success";
 function MainContent() {
   const { currentStep, steps, isSubmitted, isAnimating, isDarkMode } = useOnboarding();
   return (
-    <div className={`flex-1 bg-rsed-500 p-10`}>
+    <div className="flex-1 p-10">
       <div className="max-w-md mx-auto">
         {!isSubmitted ? (
-          // add animation to this div later
           <div
             className={`transition-all duration-300 transform ${
               isAnimating ? "-translate-x-full opacity-0" : "-translate-x-0 opacity-100"
             }
             ${currentStep === steps.length - 1 ? "shadow-[0_0_10px_rgba(200,200,200,1)] rounded-lg p-4" : ""}`}
           >
-            <h1 className="text-3xl font-bol mb-3">{steps[currentStep].question}</h1>
+            <h1 className={`text-3xl mb-3 ${isDarkMode ? "text-gray-100" : "text-black"}`}>
+              {steps[currentStep].question}
+            </h1>
             {currentStep < steps.length - 1 ? <StepContent /> : <Summary />}
             <NavigationButtons />
           </div>
