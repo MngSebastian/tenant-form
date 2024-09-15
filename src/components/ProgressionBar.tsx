1be3ea55bf2b10ab1a2c361660ebc80bb1e5799c
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2024-09-15 11:05:50 +0200

    feat: Added darkMode to ProgressionBar component.

--- src/components/ProgressionBar.tsx
+++ src/components/ProgressionBar.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ProgressionSteps from "./ProgressionSteps";
+import { useOnboarding } from "../context/OnboardingContext";
 function ProgressionBar() {
+  const { isDarkMode } = useOnboarding();
   return (
-    <div className="md:w-64 h-auto md:h-full bg-white">
-      <div className="p-0 md:p-6 border-2 w-full md:h-[800px]">
-        <h2 className="text-xl pl-12 md:pl-0 font-semibold mb-6">Onboarding Progress</h2>
+    <div className={`md:w-64 h-auto md:h-full ${isDarkMode ? "bg-gray-900" : "bg-white"}`}>
+      <div className="p-0 md:p-6 w-full md:h-[800px]">
+        <h2
+          className={`text-lg md:text-xl pl-12 md:pl-0 font-semibold mb-6 ${isDarkMode ? "text-white" : "text-black"}`}
+        >
+          Onboarding Progress
+        </h2>
         <div className="flex justify-center md:justify-start gap-6 md:gap-0 md:flex-col relative">
           <div className="absolute left-14 top-6 md:left-6 md:top-4 w-[calc(100%-110px)] h-px bg-gray-400 md:w-px md:h-[calc(100%-60px)]"></div>
           <ProgressionSteps />
