cfb8eebb4589f2000925f195431414188347714e
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2024-09-15 12:19:23 +0200

    feat: DarkMode styling added to ProgressionSteps component.

--- src/components/ProgressionSteps.tsx
+++ src/components/ProgressionSteps.tsx
@@ -3,7 +3,7 @@ import { CircleCheck } from "lucide-react";
 import { useOnboarding } from "../context/OnboardingContext";
 
 function ProgressionSteps() {
-  const { currentStep, steps, isSubmitted } = useOnboarding();
+  const { currentStep, steps, isSubmitted, isDarkMode } = useOnboarding();
   return (
     <>
       {steps.map((step, index) => (
@@ -14,19 +14,28 @@ function ProgressionSteps() {
                 index === currentStep && !isSubmitted
                   ? "bg-blue-500 text-white shadow-[0_0_10px_rgba(59,130,246,0.5)]"
                   : "shadow-[0_0_10px_rgba(220,220,220,1)] border border-gray-300 bg-white text-gray-600"
-              }`}
+              } ${isDarkMode ? "text-gray-200 border-indigo-600 bg-gray-700" : ""}
+                ${isDarkMode && index === currentStep && !isSubmitted ? "bg-indigo-800" : ""}
+              `}
             >
               {index < currentStep || isSubmitted ? (
-                <CircleCheck className="w-8 h-8 text-green-500" />
+                <CircleCheck className={`w-8 h-8 ${isDarkMode ? "text-indigo-500" : "text-green-500"}`} />
               ) : (
                 <span>{index + 1}</span>
               )}
             </div>
           </div>
           <span
-            className={`hidden md:inline ml-3 ${
-              index === currentStep && !isSubmitted ? "font-semibold text-blue-500" : "font-medium texdt-gray-600"
-            }`}
+            className={`hidden md:inline ml-3 ${isDarkMode ? "text-white" : ""}
+            ${
+              index === currentStep && !isSubmitted
+                ? isDarkMode
+                  ? "font-semibold text-indigo-500" // Text color for dark mode when current step
+                  : "font-semibold text-blue-500" // Text color for light mode when current step
+                : "font-medium text-gray-600" // Default text color for non-current steps
+            }
+            
+            `}
           >
             {step.name}
           </span>
