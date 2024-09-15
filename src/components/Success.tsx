b86079d98c55b96cdd2e6858564bf30a5d61e83d
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2024-09-15 22:27:09 +0200

    bugFix: Success page would push on the ProgressionBar and make it narrower on submit

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -4,8 +4,8 @@ import { useOnboarding } from "../context/OnboardingContext";
 function Success() {
   const { isDarkMode } = useOnboarding();
   return (
-    <div className="flex flex-col items-center w-screen  mt-20">
-      <CheckCircle className="w-16 h-16 text-green-500 mx-auto mb-4 animate-pulse" />
+    <div className="flex flex-col items-center flex-grow mt-20">
+      <CheckCircle className="w-16 h-16 text-green-500 mx-auto mb-4 animate-bounce" />
       <h2 className="text-2xl font-bol text-green-600 mb-2">Success.</h2>
       <p className={`${isDarkMode ? "text-gray-300" : "text-gray-600"} `}>
         Your information has been submitted successfully.
