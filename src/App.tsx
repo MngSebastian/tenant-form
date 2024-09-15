37686afa974059b8221783874b1241d29a4b87ff
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2024-09-15 16:58:35 +0200

    Added Initial Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import ProgressionBar from "./components/ProgressionBar";
 import MainContent from "./components/MainContent";
 import NavBar from "./components/NavBar";
 import { useOnboarding } from "./context/OnboardingContext";
+import Success from "./components/Success";
+import NotFound from "./components/NotFound";
+
 function App() {
-  const { isDarkMode } = useOnboarding();
+  const { currentStep, steps, isDarkMode } = useOnboarding();
   return (
     <div className={`flex flex-col w-screen h-screen ${isDarkMode ? "bg-slate-800" : "bg-gray-100"}`}>
       <NavBar />
       <div className="flex flex-col md:flex-grow md:flex-row">
         <ProgressionBar />
-        <MainContent />
+        <Routes>
+          <Route path="/" element={<Navigate to={`/form/${steps[currentStep].name.toLowerCase()}`} />} />
+          <Route path="/form/:step" element={<MainContent />} />
+          <Route path="/form/success" element={<Success />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   );
