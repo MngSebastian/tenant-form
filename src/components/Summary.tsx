e80b8542a176eee5761e03e7c8898ed6301f326d
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2024-09-16 14:55:42 +0200

    Changed padding to main div in MainContent to look better on mobile

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -7,10 +7,14 @@ function Summary() {
     <div className="mb-6 space-y-2">
       {Object.entries(formData).map(([key, value]) => (
         <div key={key} className="flex gap-3">
-          <span className={`${isDarkMode ? "text-gray-400" : "text-black"} font-semibold w-16 text-lg capitalize`}>
+          <span
+            className={`${
+              isDarkMode ? "text-gray-400" : "text-black"
+            } font-semibold w-16 text-md md:text-lg capitalize`}
+          >
             {key}:
           </span>
-          <span className={`${isDarkMode ? "text-gray-200" : "text-black"} text-lg`}>{value}</span>
+          <span className={`${isDarkMode ? "text-gray-200" : "text-black"} text-md md:text-lg`}>{value}</span>
         </div>
       ))}
     </div>
